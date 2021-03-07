889425a2bc051709afd5c5c57d226295bf16b6a9
Aleksei Sidorin <sidorinaleksei@gmail.com>
2021-03-07 09:44:04 +0300

    add method shouldSubmitReques

--- src/test/java/CardValidateTest.java
+++ src/test/java/CardValidateTest.java
@@ -1,7 +1,26 @@
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selectors;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class CardValidateTest {
 
+    @Test
+    void shouldSubmitRequest(){
+        open("http://localhost:9999");
+        $("[type='text']").sendKeys("Алеша");
+        $("[type='tel']").sendKeys("+79200000000");
+        $("[class='checkbox__box']").click();
+        $(byText("Продолжить")).click();
+        Configuration.timeout = 6000;
+        $(Selectors.withText("Ваша заявка успешно отправлена!")).shouldBe(visible);
+
+    }
 
 
 
