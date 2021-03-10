790cf9b0c3eb0d22e647165a0b3907ea9d0295e1
Aleksei Sidorin <sidorinaleksei@gmail.com>
2021-03-10 15:11:23 +0300

    share all methods in one branch

--- src/test/java/CardValidateTest.java
+++ src/test/java/CardValidateTest.java
@@ -19,10 +19,57 @@ public class CardValidateTest {
         $("[type='tel']").sendKeys("+79200000000");
         $("[class='checkbox__box']").click();
         $(byText("Продолжить")).click();
-        Configuration.timeout = 6000;
         $(Selectors.withText("Ваша заявка успешно отправлена!")).shouldBe(visible, Duration.ofSeconds(6));
     }
 
+    @Test
+    void shouldFieldValidBeOnName(){
+        open("http://localhost:9999");
+        $("[type='text']").sendKeys("sdf");
+        $("[type='tel']").sendKeys("+79200000010");
+        $(byText("Продолжить")).click();
+        $(Selectors.withText("Имя и Фамилия указаные неверно.")).shouldBe(visible);
+
+    }
+    @Test
+    void shouldFieldValidBeOnPhone(){
+        open("http://localhost:9999");
+        $("[type='text']").sendKeys("Витя");
+        $("[type='tel']").sendKeys("+23123");
+        $(byText("Продолжить")).click();
+        $(Selectors.withText("Телефон указан неверно.")).shouldBe(visible);
+
+    }
+
+    @Test
+    void shouldFieldValidBeOnCheckBox(){
+        open("http://localhost:9999");
+        $("[type='text']").sendKeys("Дима");
+        $("[type='tel']").sendKeys("+79200000000");
+        $(byText("Продолжить")).click();
+        $("[class='checkbox checkbox_size_m checkbox_theme_alfa-on-white input_invalid']").shouldBe(visible);
+
+    }
+
+    @Test
+    void shouldFieldValidIfNameIsEmpty(){
+        open("http://localhost:9999");
+        $("[type='text']").sendKeys("");
+        $("[type='tel']").sendKeys("+79200000000");
+        $("[class='checkbox__box']").click();
+        $(byText("Продолжить")).click();
+        $(Selectors.withText("Поле обязательно для заполнения")).shouldBe(visible);
+    }
+
+    @Test
+    void shouldFieldValidIfPhoneIsEmpty(){
+        open("http://localhost:9999");
+        $("[type='text']").sendKeys("Женя");
+        $("[type='tel']").sendKeys("");
+        $("[class='checkbox__box']").click();
+        $(byText("Продолжить")).click();
+        $(Selectors.withText("Поле обязательно для заполнения")).shouldBe(visible);
+    }
 
 
 }
